7afcb0e7164a8b1714e8252a916b7c6d1c340d00
Arun Uday <72249990+arunuday117@users.noreply.github.com>
2023-01-04 21:19:07 +0530

    Create MainActivity.java

---
+++ Adapter_excep/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.co3_pg1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    ListView l1;
+    String [] nam = {"Shajahan","Tovino","Ragav","Bineesh"};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        try {
+            ArrayAdapter<String> adap = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,nam);
+            l1.setAdapter(adap);
+        }
+        catch (Exception e){
+            Toast.makeText(this, e.getMessage(),Toast.LENGTH_SHORT).show();}
+    }
+}
